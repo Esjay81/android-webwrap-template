596701508e3de2f2d442ab15b35d76ad0a584afd
Esjay81 <shaun.jejane81@gmail.com>
2025-08-18 13:07:03 +0200

    Clean MainActivity.kt without explicit R import - use auto-generated R class

--- app/src/main/java/com/webwrap/android/MainActivity.kt
+++ app/src/main/java/com/webwrap/android/MainActivity.kt
@@ -6,7 +6,6 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.webwrap.android.R
 
 class MainActivity : AppCompatActivity() {
 
