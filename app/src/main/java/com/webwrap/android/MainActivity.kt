704156e78461f479b1fc837082758a082220100f
Esjay81 <shaun.jejane81@gmail.com>
2025-08-18 11:47:59 +0200

    Recreate MainActivity.kt with correct SuppressLint

---
+++ app/src/main/java/com/webwrap/android/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.webwrap.android
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+        webSettings.setAppCacheEnabled(true)
+
+        // Load the web app URL
+        webView.loadUrl("{{WEB_APP_URL}}")
+    }
+}
