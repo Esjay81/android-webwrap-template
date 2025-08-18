dcc65f011a76ed3c413c61af55466a88418bc2c6
Esjay81 <shaun.jejane81@gmail.com>
2025-08-18 14:27:46 +0200

    MINIMAL WORKING SOLUTION: Simplest possible WebView implementation

--- app/src/main/java/com/webwrap/android/MainActivity.kt
+++ app/src/main/java/com/webwrap/android/MainActivity.kt
@@ -1,43 +1,18 @@
 package com.webwrap.android
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    
-    private lateinit var webView: WebView
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        webView = WebView(this)
-        setContentView(webView)
-        
-        setupWebView()
-        webView.loadUrl("{{WEB_APP_URL}}")
-    }
-    
-    private fun setupWebView() {
-        val settings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.cacheMode = WebSettings.LOAD_DEFAULT
-        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-        
+        val webView = WebView(this)
         webView.webViewClient = WebViewClient()
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("{{WEB_APP_URL}}")
+        setContentView(webView)
     }
 }
